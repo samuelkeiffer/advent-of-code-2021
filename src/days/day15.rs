a67aa943453df4a1e62e06a30482ccef422ca30b
Sam <samuelkeiffer@gmail.com>
2021-12-15 02:35:36 -0500

    Day 15

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,16 +1,78 @@
 use crate::*;
+use pathfinding::prelude::astar;
 
 pub fn run() {
     part1();
     part2();
 }
 
-fn part2() {}
+fn part2() {
+    let Input(grid) = get_input::<Input>("day-15");
+    let mut big_grid = Vec::<Vec<usize>>::new();
+    for i in 0..5 {
+        let mut big_row = vec![Vec::<usize>::new(); grid.len()];
+        for j in 0..5 {
+            let mut grid_portion = grid
+                .iter()
+                .map(|row| {
+                    row.iter()
+                        .map(|risk| {
+                            let modulus = (risk + i + j) % 9;
+                            if modulus == 0 {
+                                9
+                            } else {
+                                modulus
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+            big_row
+                .iter_mut()
+                .zip(grid_portion.iter_mut())
+                .for_each(|(row, extend)| row.append(extend))
+        }
+        big_grid.append(&mut big_row);
+    }
+    let cost = find_path_cost(&big_grid);
+    dbg!(cost);
+}
 
-fn part1() {}
+fn part1() {
+    let Input(grid) = get_input::<Input>("day-15");
+    let cost = find_path_cost(&grid);
+    dbg!(cost);
+}
+
+fn find_path_cost(grid: &[Vec<usize>]) -> usize {
+    let start_point: (usize, usize) = (0, 0);
+    let end_point = (grid.len() - 1, grid[0].len() - 1);
+    let (_path, cost) = astar(
+        &start_point,
+        |(x, y)| {
+            [
+                (x + 1, *y),
+                (*x, y + 1),
+                ((*x).max(1) - 1, *y),
+                (*x, (*y).max(1) - 1),
+            ]
+            .iter()
+            .filter_map(|(x, y)| {
+                grid.get(*x)
+                    .and_then(|row| row.get(*y))
+                    .map(|cost| ((*x, *y), *cost))
+            })
+            .collect::<Vec<_>>()
+        },
+        |(x, y)| end_point.0 - x + end_point.1 - y,
+        |point| *point == end_point,
+    )
+    .unwrap();
+    cost
+}
 
 #[derive(Clone, Deserialize)]
-struct Input(Vec<u32>);
+struct Input(Vec<Vec<usize>>);
 
 impl Asset for Input {
     const EXTENSION: &'static str = "ron";
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@
 use advent_of_code::days::*;
 
 fn main() {
-    day14::run();
+    day15::run();
 }
